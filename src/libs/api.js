07982a66d1df08a7b9b3c92ebe45e536171b456e
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-03-06 22:13:45 +0530

    added eslint and prettier

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,14 +1,16 @@
-import { httpUrl } from "./vars";
+import { httpUrl } from './vars';
 
 const resource = {
   web: {
     terms: `${httpUrl}/terms`
   },
+
   api: {
-    me: `${httpUrl}/api/users/me`,
+    me: `${httpUrl}/api/users/me`
   }
 };
 
 const { api, web } = resource;
 
 export { web, api };
+
