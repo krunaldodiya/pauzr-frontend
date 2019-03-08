decf854d47242562db0aa6c67efe18f21948802a
shivanshu41 <shiv999d@gmail.com>
2019-03-08 18:46:31 +0530

    Terms and conditions page finalized

--- src/screens/termsNConditions/index.js
+++ src/screens/termsNConditions/index.js
@@ -1,31 +1,68 @@
 import React from 'react';
-import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
-import { Button } from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Body,
+  CheckBox,
+  ListItem,
+  Button
+} from 'native-base';
+import { Text } from 'react-native';
+import styles from './styles';
+import TNCText from '../../components/TncText';
 export default class TermsNConditions extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      checkbox: true
+    };
+    this.checkboxHandler = this._checkboxHandler.bind(this);
+  }
+  _checkboxHandler() {
+    if (!this.state.checkbox) {
+      this.setState({
+        checkbox: true
+      });
+    } else {
+      this.setState({
+        checkbox: false
+      });
+    }
   }
   render() {
     return (
-      <Container>
-        <Header />
-        <Content>
-          <Card>
-            <CardItem>
-              <Body>
-                <Text>Terms and condition</Text>
-              </Body>
-            </CardItem>
-          </Card>
+      <Container style={styles.container}>
+        <Header style={styles.header}>
+          
+          <Body style={styles.body}>
+            <Text style={styles.title}>Terms and Conditions</Text>
+           
+          </Body>
+          
+        </Header>
+        <Content style={styles.content}>
+          <TNCText style={{fontSize:17}}></TNCText>
         </Content>
-        <Button
-          title="Continue"
-          color="#FFC107"
-          height="58"
+        <ListItem>
+          <CheckBox
+            style={styles.checkbox}
+            checked={this.state.checkbox}
+            color={'#512DA8'}
+            onPress={() => this.checkboxHandler()}
+          />
+          <Body>
+            <Text>I accept the Terms and Conditions provided above.</Text>
+          </Body>
+        </ListItem>
+        <Button style={styles.button} full
           onPress={() => {
             this.props.navigation.push('AppIntro');
           }}
-        />
+          
+         >
+        <Text style={{fontSize:18,fontWeight:"bold",color:"#ffffff"}}>Continue</Text>
+        </Button>
       </Container>
     );
   }
--- src/screens/termsNConditions/styles.js
+++ src/screens/termsNConditions/styles.js
@@ -1,15 +1,36 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   spinner: {
-    color: "white"
+    color: 'white'
   },
-  parent:{
-    backgroundColor:"#7B1FA2",
-    color:"#ffffff",
+  container: {
+    backgroundColor: '#ffffff'
+  },
+  content: {
+    paddingTop: 10,
+    paddingRight: 30,
+    paddingLeft: 30,
+    paddingBottom: 30
+  },
+  checkbox: {
+    marginRight: 20
+  },
+  header: {
+    backgroundColor: '#ffffff'
+  },
+  body: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: '#7B1FA2'
   },
   button:{
-    position:"absolute",
-    bottom:100,
+    backgroundColor:"#7B1FA2",
+    height:58,
   }
 });
