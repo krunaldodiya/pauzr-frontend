3058397effca135ab744513a1b409a817c7e2524
shivanshu41 <shiv999d@gmail.com>
2019-03-10 23:55:32 +0530

    WEbWiew var import and ProfileJS

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import {WebView} from 'react-native';
-
+import {httpUrl} from '../../libs/vars';
 export default class LoginWebView extends Component {
   render() {
     return (
       <WebView
-        source={{uri: 'http://www.pauzr.com/login/google'}}
+        source={{uri: httpUrl+'/login/google'}}
         style={{marginTop: 20}}
       />
     );
