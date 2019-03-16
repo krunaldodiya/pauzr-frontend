4bf7a8e2b797f7b76bac0d7379837acf6ae42cc5
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-03-16 08:40:45 +0530

    started edit profile

--- src/components/User/Profile/EditProfileForm/index.js
+++ src/components/User/Profile/EditProfileForm/index.js
@@ -1,14 +1,55 @@
-import { View, Text } from "native-base";
 import React from "react";
-import styles from "./styles";
+import { KeyboardAvoidingView, SafeAreaView } from "react-native";
+import { getContacts } from "../../../services";
+import Loader from "../../Shared/Loader";
+import Content from "./content";
+import Header from "./header";
 
-const EditProfileForm = () => (
-  <View style={styles.infoWrapper}>
-    <Text style={styles.infoHeading}>
-      Please, Enter verification code send to &nbsp;
-    </Text>
-    <Text style={styles.infoParagraph}>hello</Text>
-  </View>
-);
+class ManageProfile extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default EditProfileForm;
+    this.state = {
+      avoidKeyboard: false
+    };
+  }
+
+  async componentWillMount() {
+    const { auth, updateUserContacts } = this.props;
+    const { authUser } = auth;
+
+    if (authUser.contacts.length === 0) {
+      const contacts = await getContacts();
+      updateUserContacts({ contacts });
+    }
+  }
+
+  toggleKeyboardAvoidView = avoidKeyboard => {
+    this.setState({ avoidKeyboard });
+  };
+
+  render() {
+    const { loading } = this.props;
+    const { avoidKeyboard } = this.state;
+
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <KeyboardAvoidingView
+          behavior="position"
+          enabled={avoidKeyboard}
+          style={{ flex: 1 }}
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Loader loading={loading.effects.auth.updateAuthUser} />
+          <Header {...this.props} />
+          <Content
+            {...this.props}
+            toggleKeyboardAvoidView={this.toggleKeyboardAvoidView}
+          />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default ManageProfile;
--- src/components/User/Profile/EditProfileForm/styles.js
+++ src/components/User/Profile/EditProfileForm/styles.js
@@ -1,26 +1,76 @@
-import { StyleSheet } from 'react-native';
-import theme from '../../../../libs/theme';
+import { StyleSheet } from "react-native";
+import theme from "../../../libs/theme";
 
 export default StyleSheet.create({
-  infoWrapper: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 30
+  container: {
+    flex: 1
   },
-  infoHeading: {
-    fontSize: 26,
-    textAlign: 'center',
-    color: '#fff',
-    fontFamily: theme.fonts.TitilliumWebSemiBold
+  termsWrapper: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "black"
   },
-  infoParagraph: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#fff',
-    marginHorizontal: 20,
+  termsHeader: {
+    fontWeight: "400",
+    fontFamily: theme.fonts.TitilliumWebSemiBold,
+    color: "black",
+    fontSize: 16,
+    textAlign: "center"
+  },
+  topImageWrapper: {
+    alignItems: "center",
+    padding: 50,
+    backgroundColor: "#006feb"
+  },
+  topImage: {
+    width: 120,
+    height: 120
+  },
+  formWrapper: {
+    marginTop: 20,
+    flexDirection: "column",
+    justifyContent: 'center'
+  },
+  inputWrapper: {
+    marginBottom: 15,
+    alignItems: 'center'
+  },
+  input: errors => {
+    return {
+      alignSelf: "center",
+      borderColor: errors ? "#e74c3c" : "gray",
+      width: 250,
+      padding: 5,
+      borderWidth: 1,
+      borderRadius: 20,
+      paddingLeft: 20,
+      fontFamily: theme.fonts.TitilliumWebRegular,
+      fontSize: 14
+    };
+  },
+  buttonWrapper: {
+    marginBottom: 10,
+    justifyContent: "center",
+    borderBottomWidth: 0
+  },
+  submitButtonWrapper: {
     marginTop: 30,
-    fontFamily: theme.fonts.TitilliumWebRegular
+    justifyContent: 'center',
+    alignSelf: "center",
+    borderBottomWidth: 0
+  },
+  submitButton: {
+    backgroundColor: "#006feb",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5
+  },
+  submitButtonText: {
+    textAlign: "center",
+    fontFamily: theme.fonts.TitilliumWebSemiBold,
+    fontSize: 14,
+    color: "white"
   }
 });
