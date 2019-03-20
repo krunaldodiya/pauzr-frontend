505060977dc460c5939cc825319664f3a7bcc444
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-03-20 19:00:36 +0530

    user profile update done

--- src/screens/Timer/index.js
+++ src/screens/Timer/index.js
@@ -1,25 +1,24 @@
+import { Container, Content, Header } from 'native-base';
 import React from 'react';
-import { Animated, Text, View } from 'react-native';
-import styles from './styles';
-import { Container, Header, Content } from 'native-base';
+import { Text, View } from 'react-native';
 import BottomNav from '../../components/Home/BottomNav';
 import TickTock from '../../components/Timer/TickTock';
+import styles from './styles';
+
 export default class Timer extends React.Component {
   constructor(props) {
     super(props);
   }
-  componentDidMount() {}
+
   render() {
     return (
       <Container style={styles.container}>
         <Header style={styles.header} />
         <Content contentContainerStyle={styles.content}>
           <View style={styles.timerBox}>
-            <TickTock playAnimationOnMount={true} duration={10000} style={styles.tickTock} >
-            
-            </TickTock>
-            <View style={styles.innerCircle} >
-            <Text style={styles.timerText}>22</Text>
+            <TickTock playAnimationOnMount={true} duration={10000} style={styles.tickTock} />
+            <View style={styles.innerCircle}>
+              <Text style={styles.timerText}>22</Text>
             </View>
           </View>
         </Content>
