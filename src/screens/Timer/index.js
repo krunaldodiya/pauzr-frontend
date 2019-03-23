1fc44d86b8ae1244c5a86cd6a8f5ec8c179a55e2
shivanshu41 <shiv999d@gmail.com>
2019-03-23 15:27:41 +0530

    Animation working

--- src/screens/Timer/index.js
+++ src/screens/Timer/index.js
@@ -1,9 +1,10 @@
+import { Container, Content, Header } from 'native-base';
 import React from 'react';
-import { Animated, Text, View } from 'react-native';
-import styles from './styles';
-import { Container, Header, Content } from 'native-base';
+import { Text, View,ImageBackground } from 'react-native';
 import BottomNav from '../../components/Home/BottomNav';
 import TickTock from '../../components/Timer/TickTock';
+import styles from './styles';
+import Wave from '../../components/Timer/Wave'
 export default class Timer extends React.Component {
   constructor(props) {
     super(props);
@@ -15,12 +16,27 @@ export default class Timer extends React.Component {
         <Header style={styles.header} />
         <Content contentContainerStyle={styles.content}>
           <View style={styles.timerBox}>
-            <TickTock playAnimationOnMount={true} duration={10000} style={styles.tickTock} >
+            <TickTock playAnimationOnMount={true} duration={1200000} style={styles.tickTock} >
             
             </TickTock>
-            <View style={styles.innerCircle} >
-            <Text style={styles.timerText}>22</Text>
+            <View style={styles.innerCircle}>
+            <Wave
+              style={styles.waveBall}
+              H={170}
+              waveParams={[
+            {A: 10, T: 280, fill: '#62c2ff'},
+            {A: 15, T: 240, fill: '#0087dc'},
+            {A: 20, T: 200, fill: '#1aa7ff'},
+        ]}
+        animated={true}
+    />
             </View>
+            
+            
+            
+           
+            <Text style={styles.timerText}>22</Text>
+            
           </View>
         </Content>
         <BottomNav />
